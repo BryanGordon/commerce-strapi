2803ef922d9a3f02cbafffcdfa8102f449e0b4d0
Bryan Gordon <bryanbge92@gmail.com>
2024-12-05 19:46:03 -0500

    Update component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,8 +1,20 @@
-export function Products () {
+import Prueba from './Prueba.css'
+
+export function Products ({ nameProduct, imageProduct, priceProduct }) {
   return (
-    <div>
-      Componente de productos
-    </div>
+    <>
+        <div className="product-container">
+          <picture className="product-img">
+            <img src={ import.meta.env.STRAPI_HOST +  imageProduct } alt={nameProduct} />
+          </picture>
+          <div className="info-container">
+            <span>{nameProduct}</span>
+            <span>${priceProduct}</span>
+          </div>
+        </div>
+    </>
+    
   )
 }
+
 
